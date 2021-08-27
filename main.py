983b8b136dc371fe474a2d0c3947a7ca41910f35
Justin Black <xtale4@gmail.com>
2021-08-27 22:13:30 +0300

    Добавил возможность искать по запросу

--- main.py
+++ main.py
@@ -2,13 +2,30 @@ from colorama import Fore, Back, Style
 import requests
 from bs4 import BeautifulSoup
 
-html = requests.get("https://habr.com/ru/all");
-parsered_html = BeautifulSoup(html.text, "html.parser")
 
-title_links = parsered_html.select(".tm-article-snippet__title-link")
+search_query = input("Введите запрос: ")
 
-for span in title_links:
-    print(Fore.RED + span.contents[0].contents[0] + Fore.GREEN +" (" + span["href"] + ")")
+def search_posts(text_of_query):
+    payload = {"q": search_query, "target_type": "posts", "order": "relevance"}
+    html = requests.get("https://habr.com/ru/search/", params=payload);
+    parsered_html = BeautifulSoup(html.text, "html.parser")
+
+
+    all_posts = parsered_html.select(".tm-article-snippet__title-link")
+
+    for post in all_posts:
+        title_of_post = ""
+        link_of_post = "https://habr.com" + post["href"]
+        for text in post.contents[0].strings:
+            title_of_post += text
+        
+        title_of_post = Fore.RED + title_of_post
+        link_of_post = Back.RESET + Fore.GREEN + link_of_post
+        
+        print(f"{title_of_post} \n ({link_of_post}) \n")
+
+
 
 # Нужно что бы терминал был нормального цвета
-print(Fore.WHITE) 
+search_posts(search_query)
+print(Style.RESET_ALL)
